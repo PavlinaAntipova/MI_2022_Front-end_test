0ac13d4fb43096d0876a141cfd3fef6178909265
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-07-01 16:54:32 +0300

    feat: Add markup HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import HomeImg from '../images/homePage/homeImg.png';
+import { Img } from './style/HomePage.styled';
+
+
+export default function HomePage() {
+    return <>
+        <Img src={HomeImg} alt="Girl and pet" />
+    </>
+}

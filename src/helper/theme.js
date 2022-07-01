18bfc33d79c9c9f97c4e0bd67d654471fee3d41f
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-07-01 13:27:03 +0300

    style: add basic settings

---
+++ src/helper/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+    mainAccentColor: "#FF868E",
+    secondaryAccentColor: "FBE0DC",
+    darkTextColor: "#1D1D1D",
+    lightTextColor: "#8C8C8C",
+    borderRadiusMax: "20px",
+    borderRadiusMin: "10px",
+}

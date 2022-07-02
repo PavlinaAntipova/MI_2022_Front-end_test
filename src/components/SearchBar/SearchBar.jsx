2ba460b94620a221f765e3afe3c64b8ea7360158
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-07-02 11:38:20 +0300

    feat: add murkup Search Bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,22 @@
+import { NavLink } from "react-router-dom";
+
+import { ReactComponent as SearchIcon } from '../../images/icons/search.svg';
+import { ReactComponent as LikeIcon } from '../../images/icons/like.svg';
+import { ReactComponent as FavoriteIcon } from '../../images/icons/fav.svg';
+import { ReactComponent as DislikehIcon } from '../../images/icons/dislike.svg';
+import { Btn, Input, Item, Link, List, SearchBox, StyledSearchBar } from "./SearchBar.styled";
+
 export default function SearchBar() {
 
-    return <div></div>
+    return <StyledSearchBar>
+        <SearchBox>
+            <Input type="text" placeholder="Search for breeds by name"/>
+            <Btn type="submit"><SearchIcon width="20" height="20"/></Btn>
+    </SearchBox>
+     <List>
+            <Item><Link to="/likes"><LikeIcon width="30" height="30"/></Link></Item>
+            <Item><Link to="/favourites"><FavoriteIcon width="30" height="30"/></Link></Item>
+            <Item><Link to="/dislikes"><DislikehIcon width="30" height="30"/></Link></Item>
+        </List>
+        </StyledSearchBar>
 }

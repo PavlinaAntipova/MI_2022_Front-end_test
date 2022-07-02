1eb6e08a1b8db40e9e70646bdb061608c1221dba
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-07-02 15:29:59 +0300

    Revert "fix: fix Routes"
    This reverts commit 420fc326a61f1f442c5c4b10bab1f8d921b49819.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 
 import { App } from 'components/App/App';
 import './index.css';
-import { ThemeProvider } from 'styled-components';
-import { theme } from 'helper/theme';
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
     <BrowserRouter>
       <App />
       </BrowserRouter>
-      </ThemeProvider>
   </React.StrictMode>
 );

751d7518729b9f30b2a34850ceea1017ddda400f
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-07-02 15:10:32 +0300

    feat & fix: change Layout's markup and add markup  & adjust Back Button

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,18 +1,26 @@
 import NavigationBtn from "components/NavigationBtn";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 import { ReactComponent as Logo } from '../../images/icons/logo.svg';
+import { ReactComponent as BackIcon } from '../../images/icons/back.svg';
 
 import VotingImg from '../../images/vote-table/vote-table.png';
 import BreedsImg from '../../images/pet-breeds/pet-breeds.png';
 import GalleryImg from '../../images/images-search/images-search.png';
-import { Description, Header, Item, List, StyledLayout, Text, Title, IntroBox } from "./Layout.styled";
-
+import { Description, Header, Item, List, StyledLayout, Text, Title, IntroBox, ContentBox, MainContent, BackBtnBox } from "./Layout.styled";
+import SearchBar from "components/SearchBar/SearchBar";
+import { useEffect } from "react";
 
 
 
 
 export default function Layout() {
+    const location = useLocation();
+
+    let history = createBrowserHistory({ window });
+   
+
     return <><StyledLayout>
         <IntroBox>
         <Header>
@@ -28,11 +36,19 @@ export default function Layout() {
                 <Item><NavigationBtn path="/gallery" text="Gallery" img={{ path: GalleryImg, width: 112, height: 190 }} bgColor="#FFD280"/></Item>
             </List>
         </IntroBox>
-        
-        
-        <Outlet/>
-       
-       
+
+        <MainContent>
+        {location?.pathname !== "/" && <SearchBar />}
+            <ContentBox location={location}>
+                {location?.pathname !== "/" && <BackBtnBox>
+                    <button onClick={() => { history.back() } type="button"><BackIcon/></button>
+                    <span>{location?.pathname.slice(1)}</span>
+                </BackBtnBox>}
+                
+            <Outlet/>
+            </ContentBox>
+        </MainContent>
+
     </StyledLayout>
         
         

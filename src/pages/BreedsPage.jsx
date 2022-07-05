e95aa02732802796d9ed97aae8099cdccce3bc45
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-07-05 15:24:57 +0300

    feat: add markup for Breeds Page & add fetching breeds

--- src/pages/BreedsPage.jsx
+++ src/pages/BreedsPage.jsx
@@ -1,3 +1,37 @@
+import Dropdown from "components/Dropdown";
+import { theme } from "helper/theme";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Circles } from "react-loader-spinner";
+import { getBreeds } from "services/catsApi";
+
+const limits = [
+    { value: '5', label: 'Limit: 5' },
+    { value: '10', label: 'Limit: 10' },
+    { value: '15', label: 'Limit: 15' },
+    { value: '20', label: 'Limit: 20' },
+];
+
 export default function BreedsPage() {
-    return <></>
+    const [breeds, setBreeds] = useState([]);
+    const [optionsBreeds, setOptionsBreeds] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        getBreeds().then(data => {
+            setBreeds(data);
+            const options = data.map(item => ({ value: item.id, label: item.name }));
+            setOptionsBreeds(options);
+        }).finally(() => { setIsLoading(false) });
+    }, []);
+    
+    return (<>
+        {isLoading ? <Circles height="80" width="80" color={theme.mainAccentColor} ariaLabel='loading'
+        /> : <>
+            <ul>
+                {breeds.map(item => <li key={item.id}><img src={item?.image?.url} alt="" /></li>)}
+            </ul>
+        </>}
+            </>);
 }

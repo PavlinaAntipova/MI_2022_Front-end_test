65ff2170e665f86d98b310ddc7a45f90aca86804
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-07-05 13:33:57 +0300

    style: add style for Rating Page

---
+++ src/pages/style/RatingPage.styled.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const NotFoundText = styled.p`
+padding: 18px 20px;
+background-color: #F8F8F7;
+border-radius: ${props => props.theme.borderRadiusMin};
+color: ${props => props.theme.lightTextColor};
+font-size: 16px;
+line-height: 1.5em;
+`;

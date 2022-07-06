e3a2f264085e072732d9387bd6858ed67ae72dfb
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-07-06 16:10:06 +0300

    fix: change name of icon

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import { ReactComponent as SearchIcon } from '../../images/icons/search.svg';
 import { ReactComponent as LikeIcon } from '../../images/icons/like.svg';
 import { ReactComponent as FavoriteIcon } from '../../images/icons/fav.svg';
-import { ReactComponent as DislikehIcon } from '../../images/icons/dislike.svg';
+import { ReactComponent as DislikeIcon } from '../../images/icons/dislike.svg';
 import { Btn, Input, Item, Link, List, SearchBox, StyledBtn, StyledSearchBar } from "./SearchBar.styled";
 import { theme } from "helper/theme";
 import { BasicBtn } from "components/Btn/Btn.styled";

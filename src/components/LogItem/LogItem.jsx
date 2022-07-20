e721eff67c143059a15361836057fb353f3b4674
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-07-20 17:10:32 +0300

    feat: change render depending on type of voting

--- src/components/LogItem/LogItem.jsx
+++ src/components/LogItem/LogItem.jsx
@@ -4,29 +4,38 @@ import 'moment/locale/uk';
 import { ReactComponent as LikeIcon } from '../../images/icons/like.svg';
 import { ReactComponent as FavoriteIcon } from '../../images/icons/fav.svg';
 import { ReactComponent as DislikeIcon } from '../../images/icons/dislike.svg';
-import { theme } from 'helper/theme';
+
 import { Id, Log, Text, Time } from './LogItem.styled';
+import { theme } from 'helper/theme';
 
-export default function LogItem({item}) {
-    const { image_id, value = undefined, created_at } = item;
+export default function LogItem({ item }) {
+    const { image_id, value = undefined, created_at, isDeleted = undefined } = item;
 
-
-
-    const getType = (value) => {
-        if (value) {
-            return 'added to Likes';
-        } else if (value === 0) {
-            return 'added to Dislikes';
-        } else {
+    const getTypeOfFavourite = isDeleted => {
+            
+        if (isDeleted === true) {
+            return 'removed from Favourites';
+            }
             return 'added to Favourites';
-        }
-  
     }
 
-    return <Log>
-        <Text><Time>{moment(created_at).format('LT')}</Time>Image ID: <Id>{image_id}</Id> was {getType(value)}</Text>
-        {value ? <LikeIcon fill="#97EAB9" width="20" height="20"/> : value === 0 ? <DislikeIcon fill="#FFD280" width="20" height="20"/> : <FavoriteIcon fill={theme.mainAccentColor} width="20" height="20"/>}
+    if (value === 1) return <Log>
+        <Text><Time>{moment(created_at).format('LT')}</Time>Image ID: <Id>{image_id}</Id> was added to Likes</Text>
+        <LikeIcon fill="#97EAB9" width="20" height="20"/>
+    </Log>
+    
+    if (value === 0) return <Log>
+        <Text><Time>{moment(created_at).format('LT')}</Time>Image ID: <Id>{image_id}</Id> was added to Dislikes</Text>
+        <DislikeIcon fill="#FFD280" width="20" height="20"/>
+    </Log>
+    
+    if (value === undefined) return <Log>
+        <Text><Time>{moment(created_at).format('LT')}</Time>Image ID: <Id>{image_id}</Id> was {getTypeOfFavourite(isDeleted)}</Text>
+        {item?.isDeleted ? null : <FavoriteIcon fill={theme.mainAccentColor} width="20" height="20"/>}
     </Log>
 
-}
+    } 
+    
+
+
  

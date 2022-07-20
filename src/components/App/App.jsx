92d851e9632409e24580f17d698e123f78986b61
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-07-20 17:06:42 +0300

    feat: add new Routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,21 @@
+import Loader from "components/Loader";
+import { useState, useEffect,lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import { toast, ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+import { getBreeds } from "services/catsApi";
+
+import Layout from "components/Layout";
+const BreedsPage = lazy(() => import("pages/BreedsPage"));
+const GalleryPage = lazy(() => import("pages/GalleryPage"));
+const HomePage = lazy(() => import("pages/HomePage"));
+const RatingPage = lazy(() => import("pages/RatingPage"));
+const SearchPage = lazy(() => import("pages/SearchPage"));
+const VotingPage = lazy(() => import("pages/VotingPage"));
+const BreedInfoPage = lazy(() => import("pages/BreedInfoPage"));
+
 
 export const App = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -10,13 +27,28 @@ export const App = () => {
             toast("🙀 Ooops, something went wrong! Try again.");
         })
   }, []);
+
+  return (<>
+    <Suspense fallback={<Loader />}>
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Layout setSearchQuery={setSearchQuery} />}>
         <Route index element={<HomePage />} />
         <Route path="voting" element={<VotingPage />} />
-        <Route path="breeds" element={<BreedsPage />} />
-        <Route path="gallery" element={<GalleryPage />} />
+        <Route path="breeds" element={<BreedsPage breeds={breeds} />}>
+          <Route path=":breedId" element={<BreedInfoPage/>} />
+          </Route>
+        <Route path="gallery" element={<GalleryPage breeds={breeds} />} />
+        <Route path="likes" element={<RatingPage />}  />
+        <Route path="dislikes" element={<RatingPage />} />
+        <Route path="favourites" element={<RatingPage />} />
+        <Route path="search" element={<SearchPage searchQuery={searchQuery} breeds={breeds} />} />
       </Route>
-    </Routes>
+      </Routes>
+      </Suspense>
+    <ToastContainer/>
+    </>
   );
 };
+
+
+

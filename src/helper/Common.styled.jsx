777d4136ae3d1df12ed6796fa97c311da0f4800a
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-07-20 17:04:40 +0300

    style: add basic style fortext

---
+++ src/helper/Common.styled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const BasicText = styled.p`
+font-weight: 400;
+font-size: 20px;
+line-height: 1.45em;
+color: ${props => props.theme.lightTextColor};
+`;
+

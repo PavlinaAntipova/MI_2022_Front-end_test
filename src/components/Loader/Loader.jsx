40a690af8356d6d15620a93df4da4a63b1ba0afc
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-07-20 16:23:59 +0300

    refactor: clean code Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,8 @@
+import { Circles } from "react-loader-spinner"
 import { theme } from "helper/theme";
-import { Circles } from "react-loader-spinner";
 
 export default function Loader() {
+
     return <Circles height="80" width="80" color={theme.mainAccentColor} ariaLabel='loading'
         />
  }

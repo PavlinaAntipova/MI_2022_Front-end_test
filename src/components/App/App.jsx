6b40d96ad40472103af8597b5d0a95fe573135b5
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-07-21 04:06:31 +0300

    fix: change loading chunks & add local storage for user id

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,51 +2,79 @@ import Loader from "components/Loader";
 import { useState, useEffect,lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { toast, ToastContainer } from 'react-toastify';
+import { nanoid } from 'nanoid';
+import { ThemeProvider } from "styled-components";
 
 import 'react-toastify/dist/ReactToastify.css';
 
 import { getBreeds } from "services/catsApi";
 
 import Layout from "components/Layout";
-const BreedsPage = lazy(() => import("pages/BreedsPage"));
-const GalleryPage = lazy(() => import("pages/GalleryPage"));
-const HomePage = lazy(() => import("pages/HomePage"));
-const RatingPage = lazy(() => import("pages/RatingPage"));
-const SearchPage = lazy(() => import("pages/SearchPage"));
-const VotingPage = lazy(() => import("pages/VotingPage"));
-const BreedInfoPage = lazy(() => import("pages/BreedInfoPage"));
+import BreedsPage from "pages/BreedsPage";
+import GalleryPage from "pages/GalleryPage";
+import HomePage from "pages/HomePage";
+import RatingPage from "pages/RatingPage";
+import SearchPage from "pages/SearchPage";
+import VotingPage from "pages/VotingPage";
+import BreedInfoPage from "pages/BreedInfoPage";
 
+import { themeLight, themeDark } from "helper/theme";
+import { StyledApp } from "./App.styled";
 
 export const App = () => {
+  const [isDarkTheme, setDarkTheme] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const [breeds, setBreeds] = useState([]);
+  const [userId, setUserId] = useState('');
 
   useEffect(() => {
+    const userId = JSON.parse(localStorage.getItem('user_id'));
+    const theme = JSON.parse(localStorage.getItem('theme'));
+
+    if (userId === null) {
+      const id = nanoid();
+      localStorage.setItem('user_id', JSON.stringify(id));
+      setUserId(id);
+    } else {
+      setUserId(userId);
+    }
+    if (theme === null) {
+      localStorage.setItem('theme', JSON.stringify(isDarkTheme ? "dark" : "light"));
+    } else {
+      theme === "dark" ? setDarkTheme(true) : setDarkTheme(false);
+    }
+    
     getBreeds().then(setBreeds).catch(err => {
             console.log(err.message);
             toast("🙀 Ooops, something went wrong! Try again.");
         })
   }, []);
 
-  return (<>
-    <Suspense fallback={<Loader />}>
+  useEffect(() => {
+    localStorage.setItem('theme', JSON.stringify(isDarkTheme ? "dark" : "light"));
+  },[isDarkTheme]);
+
+
+  return (
+    <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
+    <StyledApp>
     <Routes>
-      <Route path="/" element={<Layout setSearchQuery={setSearchQuery} />}>
+            <Route path="/" element={<Layout setSearchQuery={setSearchQuery} setDarkTheme={setDarkTheme} isDarkTheme={isDarkTheme} />}>
         <Route index element={<HomePage />} />
-        <Route path="voting" element={<VotingPage />} />
+          <Route path="voting" element={<VotingPage userId={userId} />} />
         <Route path="breeds" element={<BreedsPage breeds={breeds} />}>
-          <Route path=":breedId" element={<BreedInfoPage/>} />
+              <Route path=":breedId" element={<BreedInfoPage isDarkTheme={isDarkTheme} />} />
           </Route>
-        <Route path="gallery" element={<GalleryPage breeds={breeds} />} />
-        <Route path="likes" element={<RatingPage />}  />
-        <Route path="dislikes" element={<RatingPage />} />
-        <Route path="favourites" element={<RatingPage />} />
+              <Route path="gallery" element={<GalleryPage breeds={breeds} userId={userId} isDarkTheme={isDarkTheme} />} />
+        <Route path="likes" element={<RatingPage userId={userId}/>}  />
+        <Route path="dislikes" element={<RatingPage userId={userId}/>} />
+        <Route path="favourites" element={<RatingPage userId={userId}/>} />
         <Route path="search" element={<SearchPage searchQuery={searchQuery} breeds={breeds} />} />
       </Route>
       </Routes>
-      </Suspense>
-    <ToastContainer/>
-    </>
+      <ToastContainer />
+      </StyledApp>
+      </ThemeProvider>
   );
 };
 

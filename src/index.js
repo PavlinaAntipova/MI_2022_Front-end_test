9ced4c3bf4a4166c21f56dfcdcf8e3ccb171f8e3
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-07-21 04:08:22 +0300

    fix: remove Theme Provider from index.js

--- src/index.js
+++ src/index.js
@@ -5,15 +5,13 @@ import { BrowserRouter } from "react-router-dom";
 import { App } from 'components/App/App';
 import './index.css';
 
-import { ThemeProvider } from 'styled-components';
-import { theme } from 'helper/theme';
+
+import { StrictMode } from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-    <ThemeProvider theme={theme}>
+  
     <BrowserRouter basename='/MI_2022_Front-end_test/'>
       <App />
       </BrowserRouter>
-      </ThemeProvider>
-</React.StrictMode>
+
 );

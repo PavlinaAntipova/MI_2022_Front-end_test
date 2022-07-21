2373fce5acf8d97e90b58af54572d07c9bc617b7
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-07-21 23:01:37 +0300

    chore: remove Router basename

--- src/index.js
+++ src/index.js
@@ -5,12 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import { App } from 'components/App/App';
 import './index.css';
 
-
-import { StrictMode } from 'react';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <BrowserRouter basename='/MI_2022_Front-end_test/'>
+    <BrowserRouter>
       <App />
       </BrowserRouter>
 
